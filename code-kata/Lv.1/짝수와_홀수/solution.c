aa42d3918673ae4bc0f7447adc27e5959aecccb9
DoroNyong <62960758+DoroNyong@users.noreply.github.com>
2026-09-03 09:52:49 +0900

    Add solution function for even/odd determination
    Implement solution function to determine if a number is even or odd.

---
+++ code-kata/Lv.1/짝수와_홀수/solution.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int num) {
+    // 리턴할 값은 메모리를 동적 할당해주세요
+    char* answer = (char *)malloc(5 * sizeof(char));
+    sprintf(answer, "%s", (num % 2 == 0) ? "Even" : "Odd");
+    return (answer);
+}
