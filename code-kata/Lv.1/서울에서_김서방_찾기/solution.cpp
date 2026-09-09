3c70e0adeb8297931afd6c8a80cf16ba7772c4fa
DoroNyong <62960758+DoroNyong@users.noreply.github.com>
2026-09-09 09:42:38 +0900

    Implement solution to find Kim's location in Seoul

---
+++ code-kata/Lv.1/서울에서_김서방_찾기/solution.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+    int i = 0;
+    
+    for (i; i < seoul.size(); ++i)
+    {
+        if (seoul[i].compare("Kim") == 0)
+            break ;
+    }
+    
+    answer += "김서방은 " + to_string(i) + "에 있다";
+    
+    return (answer);
+}
