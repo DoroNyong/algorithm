427cec7ab01cfe4e755896373e29073673fa639b
DoroNyong <62960758+DoroNyong@users.noreply.github.com>
2026-09-14 09:48:20 +0900

    Add string validation function in solution.c
    Implement solution function to validate string length and digits.

---
+++ code-kata/Lv.2/문자열_다루기_기본/solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+bool solution(const char* s) {
+    bool answer = true;
+    size_t s_len = strlen(s);
+    
+    if (!(s_len == 4 || s_len == 6))
+        answer = false;
+    
+    for (size_t i = 0; i < s_len; ++i)
+    {
+        if (!isdigit(s[i]))
+            answer = false;
+    }
+    return (answer);
+}
